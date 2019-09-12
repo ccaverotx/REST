1817e3d251ed2e5f9d77ff091b12be190f14071c
Carlos Cavero <54409608+ccaverotx@users.noreply.github.com>
2019-09-12 15:07:11 -0400

    Create TelefApplication.java

---
+++ rest-master/nonrest/src/main/java/TelefApplication.java
@@ -0,0 +1,12 @@
+package telef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefApplication {
+
+	public static void main(String... args) {
+		SpringApplication.run(TelefApplication.class, args);
+	}
+}
