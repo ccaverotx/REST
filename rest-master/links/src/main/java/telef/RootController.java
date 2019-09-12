f3751d1db9a3f586a66d06566ca3ab37a1b83fdf
Carlos Cavero <54409608+ccaverotx@users.noreply.github.com>
2019-09-12 17:38:41 -0400

    Create RootController.java

---
+++ rest-master/links/src/main/java/telef/RootController.java
@@ -0,0 +1,20 @@
+package payroll;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class RootController {
+
+	@GetMapping
+	ResourceSupport index() {
+		ResourceSupport rootResource = new ResourceSupport();
+		rootResource.add(linkTo(methodOn(CabinaController.java).all()).withRel("cabinas"));
+		rootResource.add(linkTo(methodOn(TripController.java).all()).withRel("trips"));
+		return rootResource;
+	}
+
+}
