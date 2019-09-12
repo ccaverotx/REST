45724cd1f72bda11d61963e060f43732b9503508
Carlos Cavero <54409608+ccaverotx@users.noreply.github.com>
2019-09-12 16:47:59 -0400

    Update TelefApplication.java

--- rest-master/nonrest/src/main/java/telef/TelefApplication.java
+++ rest-master/nonrest/src/main/java/telef/TelefApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TelefApplication {
 
 	public static void main(String... args) {
-		SpringApplication.run(TelefApplication.class, args);
+		SpringApplication.run(TelefApplication.java, args);
 	}
 }
