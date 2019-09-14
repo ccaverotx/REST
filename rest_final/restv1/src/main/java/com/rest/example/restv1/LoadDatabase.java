902e42d40defb724c3eb0a2c86d2f9c2a1f7a863
Carlos Cavero <ccaverotx@icloud.com>
2019-09-13 22:53:23 -0400

    Database and some other stuff

---
+++ rest_final/restv1/src/main/java/com/rest/example/restv1/LoadDatabase.java
@@ -0,0 +1,20 @@
+package com.rest.example.restv1;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(PasajeroRepo repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Pasajero("Juan Perez", "Estudiante")));
+            log.info("Preloading " + repository.save(new Pasajero("Luis Ramirez", "Ingeniero")));
+        };
+    }
+}
---
+++ rest_final/restv1/src/main/java/com/rest/example/restv1/restv1Aplication.java
@@ -0,0 +1,12 @@
+package com.rest.example.restv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class restv1Aplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(restv1Aplication.class, args);
+    }
+}
